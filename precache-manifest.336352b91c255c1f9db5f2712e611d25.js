658c358df28b3c4221fe58591ea80a05c1d800a4
ParanoidMasky <58386674+ParanoidMasky@users.noreply.github.com>
2020-03-09 01:32:16 -0600

    Updates

---
+++ precache-manifest.336352b91c255c1f9db5f2712e611d25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d988a15a17e1519eca309190d8de1b",
+    "url": "/TPWICDWY/index.html"
+  },
+  {
+    "revision": "7a5ab01f1f02c7e2423d",
+    "url": "/TPWICDWY/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4a52ec6c05cc892f90cc",
+    "url": "/TPWICDWY/static/css/main.9e74ccc8.chunk.css"
+  },
+  {
+    "revision": "7a5ab01f1f02c7e2423d",
+    "url": "/TPWICDWY/static/js/2.55584bfc.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/TPWICDWY/static/js/2.55584bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a52ec6c05cc892f90cc",
+    "url": "/TPWICDWY/static/js/main.4bb371d3.chunk.js"
+  },
+  {
+    "revision": "5a568ddbc150a1be2e44",
+    "url": "/TPWICDWY/static/js/runtime-main.61cfcac6.js"
+  }
+]);
